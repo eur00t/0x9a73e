16f67bd4a2b54fd7c95122a857982f5c5725ceb7
Taras Yevtushenko <taras.evt@gmail.com>
2021-06-10 19:57:59 +0300

    Copy to clipboard

--- client/components/EtherscanLink.js
+++ client/components/EtherscanLink.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
+import copyToClipboard from "copy-to-clipboard";
 
 import { useNetwork } from "../utils/networks";
 import BoxArrowUpRight from "../icons/box-arrow-up-right.svg";
+import Clipboard from "../icons/clipboard.svg";
+import ClipboardCheck from "../icons/clipboard-check.svg";
+
+const CopyToClipboard = ({ value }) => {
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    if (!clicked) {
+      return;
+    }
+
+    const timerId = setTimeout(() => {
+      setClicked(false);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [clicked]);
+
+  const onClick = () => {
+    copyToClipboard(value);
+    setClicked(true);
+  };
+  return (
+    <div className="p-2 d-flex" onClick={onClick} style={{ cursor: "pointer" }}>
+      {clicked ? <ClipboardCheck /> : <Clipboard />}
+    </div>
+  );
+};
 
 export const EtherscanLink = ({
   type,
@@ -40,10 +71,12 @@ export const EtherscanLink = ({
       >
         {!showFullId ? `${id.slice(0, 10)}...` : id}
       </a>
-      <BoxArrowUpRight
-        className="ms-1 me-1"
-        style={{ position: "relative", top: "-2px" }}
-      />
+      <div className="p-2 d-flex" style={{ position: "relative", top: "-2px" }}>
+        <BoxArrowUpRight />
+      </div>
+      <div className="d-flex" style={{ position: "relative", top: "-2px" }}>
+        <CopyToClipboard value={id} />
+      </div>
     </div>
   );
 };
