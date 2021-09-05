e19d8962a039043ec60db33c4ee3a464e850e21c
Taras Yevtushenko <taras.evt@gmail.com>
2021-09-05 12:47:02 +0300

    Add Matic on wrong network route

--- client/routes/WrongNetworkRoute.js
+++ client/routes/WrongNetworkRoute.js
@@ -1,16 +1,35 @@
 import React from "react";
 
-import { useSupportedNetworks } from "../utils/networks";
+import {
+  useSupportedNetworks,
+  getNetwork,
+  MAINNET_ID,
+} from "../utils/networks";
 import { Page } from "../components/Page";
+import { useWeb3Auth } from "../components/Web3Auth";
 
 export const WrongNetworkRoute = () => {
   const networks = useSupportedNetworks();
 
+  const { addNetwork } = useWeb3Auth();
+  const mainNetwork = getNetwork(MAINNET_ID);
+
   return (
     <Page>
-      <p>
-        This app only works with the following networks: {networks.join(", ")}.
-      </p>
+      <div className="alert alert-warning" style={{ maxWidth: "500px" }}>
+        <p>
+          This app only works with the following networks: {networks.join(", ")}
+          .
+        </p>
+        <p className="mb-0">
+          <span
+            className="btn btn-sm btn-outline-warning"
+            onClick={() => addNetwork(MAINNET_ID)}
+          >
+            Switch to {mainNetwork.name}
+          </span>
+        </p>
+      </div>
     </Page>
   );
 };
